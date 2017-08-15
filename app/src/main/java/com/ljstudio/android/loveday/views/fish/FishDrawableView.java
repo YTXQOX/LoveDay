7ccc59b4e3d531d94b2b1c93e632a4e0bc36a718
Develop <Develop>
2017-08-15 11:30:28 +0800

    init

--- app/src/main/java/com/ljstudio/android/loveday/views/fish/FishDrawableView.java
+++ app/src/main/java/com/ljstudio/android/loveday/views/fish/FishDrawableView.java
@@ -14,7 +14,6 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.view.WindowManager;
